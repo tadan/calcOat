8ec0791c6811561d2fbaee087e32810af1819371
Daniele <daniele@anothertomorrow.io>
2025-12-14 12:21:30 +0100

    feat: installed Vercel Speed Insight to check the feature

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,15 +1,21 @@
-import Container from "@/components/layout/Container";
-import LoadingSpinner from "@/components/ui/LoadingSpinner";
+import Container from '@/components/layout/Container'
+import LoadingSpinner from '@/components/ui/LoadingSpinner'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 export default function Loading() {
-  return (
-    <div className="min-h-screen bg-white py-16">
-      <Container>
-        <div className="mx-auto max-w-2xl border-5 border-black bg-oatly-cream p-12 text-center">
-          <LoadingSpinner size="lg" className="mb-6" />
-          <p className="font-mono text-lg uppercase">Loading...</p>
-        </div>
-      </Container>
-    </div>
-  );
+    return (
+        <>
+            <SpeedInsights />
+            <div className='min-h-screen bg-white py-16'>
+                <Container>
+                    <div className='mx-auto max-w-2xl border-5 border-black bg-oatly-cream p-12 text-center'>
+                        <LoadingSpinner size='lg' className='mb-6' />
+                        <p className='font-mono text-lg uppercase'>
+                            Loading...
+                        </p>
+                    </div>
+                </Container>
+            </div>
+        </>
+    )
 }
